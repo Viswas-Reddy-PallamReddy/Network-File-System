381f3a6bb3e371f04b80b769635aa957a56a72a8
Nikhil_r_8074 <156401572+Nikhil8074@users.noreply.github.com>
2024-11-17 16:25:04 +0530

    client updated

--- client.c
+++ client.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#define CHUNK_SIZE 1024 // Define the size of each data chunk
+#define CHUNK_SIZE 256 // Define the size of each data chunk
+#define MAX_ACCESSIBLE_PATHS 100
+#define MAX_FILE_SIZE 4096
 
 // Define the packet structure
 typedef struct packet
@@ -13,64 +15,75 @@ typedef struct packet
     char data[CHUNK_SIZE]; // Data chunk
 } packet;
 
-#define NM_IP "127.0.0.1"
-#define NM_PORT 9090
+char *NM_IP;
+int NM_PORT;
 #define BUFFER_SIZE 1024
 
 void play_audio_stream(int server_socket);
-void handle_nm(char *input);
+void handle_nm(char *input,int nm_socket);
 void handle_ss(char *input,char *buff);
 
-int main()
+int main(int argc,char* argv[])
 {
+    if(argc<3)
+    {
+        fprintf(stderr, "Usage: %s <port> <ip>\n", argv[0]);
+        return 1;
+    }
+    NM_IP = argv[2];
+    NM_PORT = atoi(argv[1]);
+    printf("%s %d\n",NM_IP,NM_PORT);
+   
     while(1)
     {
+        int nm_socket;
+        struct sockaddr_in nm_addr;
+        char buffer[BUFFER_SIZE];
+
+        // Create socket
+        nm_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (nm_socket == -1)
+        {
+            perror("Socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Set up naming server address
+        nm_addr.sin_family = AF_INET;
+        nm_addr.sin_port = htons(NM_PORT);
+        nm_addr.sin_addr.s_addr = inet_addr(NM_IP);
+
+        // Connect to naming server
+        if (connect(nm_socket, (struct sockaddr *)&nm_addr, sizeof(nm_addr)) < 0)
+        {
+            perror("Connection to naming server failed");
+            close(nm_socket);
+            exit(EXIT_FAILURE);
+        }
         char input[BUFFER_SIZE];
         printf("Enter command (read, write, delete, create, list, stream) and path: ");
         if (fgets(input, BUFFER_SIZE, stdin) != NULL)
         {
-            input[strcspn(input, "\n")] = 0;
+            input[strcspn(input, "\n")] = '\0';
             if(strncmp(input,"EXIT",4)==0)
                 break;
-            handle_nm(input);
+            handle_nm(input,nm_socket);
         }
+        close(nm_socket);
     }
     return 0;
 }
 
-void handle_nm(char *input)
+void handle_nm(char *input,int nm_socket)
 {
-    int nm_socket;
-    struct sockaddr_in nm_addr;
     char buffer[BUFFER_SIZE];
-
-    // Create socket
-    nm_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (nm_socket == -1)
-    {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up naming server address
-    nm_addr.sin_family = AF_INET;
-    nm_addr.sin_port = htons(NM_PORT);
-    nm_addr.sin_addr.s_addr = inet_addr(NM_IP);
-
-    // Connect to naming server
-    if (connect(nm_socket, (struct sockaddr *)&nm_addr, sizeof(nm_addr)) < 0)
-    {
-        perror("Connection to naming server failed");
-        close(nm_socket);
-        exit(EXIT_FAILURE);
-    }
-
     // Send command to naming server
     snprintf(buffer, BUFFER_SIZE, "%s", input);
     send(nm_socket, buffer, strlen(buffer), 0);
-
+    char temp_store[MAX_ACCESSIBLE_PATHS][MAX_FILE_SIZE];
     // Receive response from naming server
-    int bytes_received = recv(nm_socket, buffer, BUFFER_SIZE, 0);
+    int bytes_received;
+    bytes_received = recv(nm_socket, buffer, BUFFER_SIZE, 0);
 
     if (bytes_received > 0)
     {
@@ -78,20 +91,20 @@ void handle_nm(char *input)
         if(strncmp(input,"READ",4)==0 || strncmp(input,"WRITE",5)==0 || strncmp(input,"STREAM",6)==0 || strncmp(input,"GET_INFO",8)==0)
         {
             handle_ss(input,buffer);
+            close(nm_socket);
             return;
         }
         printf("Naming Server: %s\n", buffer);
     }
     // PENDING CODE FOR CREATE ,DELETE, LIST BY CHECkING NM CODE
-    close(nm_socket);
     
 }
 void handle_ss(char *input,char *buff)
 {
     char ss_ip[INET_ADDRSTRLEN];
     int ss_port;
-    sscanf(buff, "IP: %s\nClientPort: %d\n", ss_ip, &ss_port);
-
+    printf("%s\n",buff);
+    sscanf(buff, "%s %d", ss_ip, &ss_port);
     // Connect to storage server
     int ss_socket;
     struct sockaddr_in ss_addr;
@@ -128,6 +141,10 @@ void handle_ss(char *input,char *buff)
                     break;
                 }
                 printf("%s\n",pkt.data);
+            }
+            else
+            {
+                break;
             }
         }
     }
@@ -172,6 +189,7 @@ void handle_ss(char *input,char *buff)
             send(ss_socket, &pkt, sizeof(pkt), 0);
         }
         char buffer[BUFFER_SIZE];
+        memset(buffer,0,BUFFER_SIZE);
         recv(ss_socket, buffer, BUFFER_SIZE, 0);
         printf("%s\n", buffer);
     }
