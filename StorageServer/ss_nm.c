b4b3afad8ac744b9f8ff72ad82b5871c813ccbdd
Viswas-Reddy-PallamReddy <155218470+Viswas-Reddy-PallamReddy@users.noreply.github.com>
2024-11-20 00:12:36 +0530

    Final Code

--- StorageServer/ss_nm.c
+++ StorageServer/ss_nm.c
@@ -1,8 +1,7 @@
 #include "headers.h"
 #include "SS_NM.h"
 
-#define NM_PORT 8081
-
+#define NM_PORT 5642
 
 int connect_to_nm(int nm_port,const char* ip_address) {
     int nm_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -11,13 +10,12 @@ int connect_to_nm(int nm_port,const char* ip_address) {
     nm_addr.sin_port = htons(nm_port);
     
     if (inet_pton(AF_INET, ip_address, &nm_addr.sin_addr) <= 0) {
-        perror("Invalid IP address format for Name Manager");
+        perror("133 : Invalid IP address format for Naming Server");
         close(nm_socket);
         return -1;
     }
-
     if (connect(nm_socket, (struct sockaddr*)&nm_addr, sizeof(nm_addr)) < 0) {
-        perror("Failed to connect to Name Manager");
+        perror("134 : Failed to connect to Naming Server");
         return -1;
     }
     return nm_socket;
@@ -33,15 +31,18 @@ void initialise_to_nm(int nm_socket,const char* ip_address){
     for(int i = 0; i < this.num_accessible_paths; i++){
         strcpy(this.accessible_paths[i],"");
     }
+    
     char base[1024];
     getcwd(base, sizeof(base));
     printf("base dir: %s\n",base);
+    strcat(base,"/Paths");
     traverse_directory(base, base);
     printf("num_accessible_paths: %d\n",this.num_accessible_paths);
     char temp[MAX_PATH_LEN*this.num_accessible_paths];
     char details[1024];
     sprintf(details, "%s %d %d %d",this.ip,this.nm_port,this.client_port,this.num_accessible_paths);
     send(nm_socket, details,sizeof(details),0);
+    usleep(5000);
     for(int i = 0; i < this.num_accessible_paths; i++){
         printf("%s\n",this.accessible_paths[i]);
         strcat(temp,this.accessible_paths[i]);
@@ -59,7 +60,7 @@ void *handle_nm_thread(void *arg) {
     int nm_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (nm_socket < 0)
     {
-        perror("Error in creating client socket");
+        perror("137: Error in creating nm socket");
         exit(1);
     }
     client_addr.sin_family = AF_INET;
@@ -67,40 +68,40 @@ void *handle_nm_thread(void *arg) {
     client_addr.sin_addr.s_addr = INADDR_ANY;
     if (bind(nm_socket, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0)
     {
-        perror("Error in binding client socket");
+        perror("138 :Error in binding nm socket");
         exit(1);
     }
     int listen_status = listen(nm_socket,100);
     if (listen_status < 0)
     {
-        perror("Error in listening to client socket");
+        perror("139 : Error in listening to nm socket");
         exit(1);
     }
     while (1)
     {
-        printf("Waiting for client connection\n");
-        int accept_status = accept(nm_socket, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (accept_status < 0)
+        printf("Waiting for nm connection\n");
+        final_nm_socket = accept(nm_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (final_nm_socket < 0)
         {
-            perror("Error in accepting connection from client");
+            perror("Error in accepting connection from Naming Server");
             exit(1);
         }
         pthread_t thread;
-        pthread_create(&thread, NULL, process_requests, &accept_status);
+        pthread_create(&thread, NULL, process_requests, NULL);
         // pthread_detach(thread);
     }
 }
 
 void *process_requests(void *arg) {
-    int nm_socket = *(int *)arg;
+
     char buffer[BUFFER_SIZE];
     memset(buffer, 0, BUFFER_SIZE);
 
     while(1){
-        int bytes_received = recv(nm_socket, buffer, BUFFER_SIZE, 0);
-        if (bytes_received <= 0) {
+        int bytes_received = recv(final_nm_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received < 0) {
             perror("Failed to receive command from NM");
-            close(nm_socket);
+            close(final_nm_socket);
             break;
         }
         printf("Received command:%s\n",buffer);
@@ -110,7 +111,7 @@ void *process_requests(void *arg) {
             break;
         }
         else{
-            handle_nm(nm_socket,buffer);
+            handle_nm(buffer);
         }
         // send(nm_socket, "Received", strlen("Received"), 0);
     }
@@ -118,49 +119,55 @@ void *process_requests(void *arg) {
     pthread_exit(NULL);
 }
 
-void handle_nm(int nm_socket, char *nm_command) {
+void handle_nm(char *nm_command) {
     printf("Received command from nm: %s\n", nm_command);
     char* command = strtok(nm_command, " ");
     printf("command: %s\n",command);
-
     if(strcmp(command, "CREATE")==0){
         char* type = strtok(NULL, " ");
         printf("type: %s\n",type);
-        char* path = strtok(NULL, " ");
+        char* temp = strtok(NULL, " ");
+        printf("temp: %s\n",temp);
+        char  path[MAX_PATH_LEN];
+        snprintf(path, strlen(temp)+9, "./Paths/%s/", temp+2);
         printf("path: %s\n",path);
         char* filename = strtok(NULL, " ");
+        strcat(path, filename);
         printf("filename: %s\n",filename);
-        char full_path[MAX_PATH_LEN];
-        snprintf(full_path, sizeof(full_path), "%s/%s", path, filename);
         if(strcmp(type, "file")==0){
-            create_file(nm_socket, full_path);
+            create_file(path);
         }
         else if(strcmp(type, "folder")==0){
-            create_folder(nm_socket, full_path);
+            create_folder(path);
         }
     }
-
     else if(strcmp(command, "DELETE")==0){
-        char* filename = strtok(NULL, " ");
-        printf("file to delete: %s\n",filename);
+        char* temp = strtok(NULL, " ");
+         char filename[BUFFER_SIZE];
+        snprintf(filename, BUFFER_SIZE, "Paths/%s", temp+2);
+        printf("file to delete:%s\n",filename);
         DIR* dir = opendir(filename);
         if (dir == NULL) {
-            delete_file(nm_socket,filename);
+            delete_file(filename);
         } else {
-            delete_folder(nm_socket,filename);
+            delete_folder(filename);
         }
         closedir(dir);
     }
     else if(strcmp(command, "READ")==0){
-        char* filename = strtok(NULL, " ");
-        read_file(nm_socket, filename);
+        char* temp = strtok(NULL, " ");
+        char filename[BUFFER_SIZE];
+        snprintf(filename, BUFFER_SIZE, "Paths/%s", temp+2);
+        read_file(final_nm_socket,filename);
     }
     else if(strcmp(command, "WRITE")==0){
-        char* filename = strtok(NULL, " ");
-        write_to_file(nm_socket, filename,1);
+        char* temp = strtok(NULL, " ");
+        char filename[BUFFER_SIZE];
+        snprintf(filename, BUFFER_SIZE, "Paths/%s", temp+2);
+        write_to_file(final_nm_socket,filename,1);
     }
     else{
-        send(nm_socket, "Invalid command\n", 16, 0);
+        send(final_nm_socket, "ERROR 1", 10, 0);
     }
 
 }
@@ -178,6 +185,7 @@ void traverse_directory(const char *dirname, const char *base) {
             }
 
             char full_path[1024];
+            
             snprintf(full_path, sizeof(full_path), "%s/%s", dirname, ent->d_name);
 
             // Get the file status
@@ -193,8 +201,12 @@ void traverse_directory(const char *dirname, const char *base) {
 
                 // Recursive call for subdirectories
                 if (S_ISDIR(st.st_mode)) {
+                    // strcat(relative_path, " folder");
                     traverse_directory(full_path, base);
                 }
+                // else{
+                //     strcat(relative_path, " file");
+                // }
                 strcpy(this.accessible_paths[this.num_accessible_paths], relative_path);
                 this.num_accessible_paths++;
             }
@@ -205,11 +217,12 @@ void traverse_directory(const char *dirname, const char *base) {
     }
 }
 
-void create_file(int nm_socket, const char *filepath) {
+void create_file(const char *filepath) {
     FILE *temp_file = fopen(filepath, "r");
     if (temp_file != NULL) {
         printf("File already exists\n");
-        send(nm_socket, "File already exists\n", 20, 0);
+        // send(nm_socket, "File already exists\n", 20, 0);
+        send(final_nm_socket, "ERROR 5", 10, 0);
         fclose(temp_file);
         return; 
     }
@@ -218,42 +231,50 @@ void create_file(int nm_socket, const char *filepath) {
     FILE *file = fopen(filepath, "w");
     if (!file) {
         printf("Failed to create file\n");
-        send(nm_socket, "Failed to create file\n", 23, 0);
+        // send(nm_socket, "Failed to create file\n", 23, 0);
+        send(final_nm_socket, "ERROR 7", 10, 0);
         return;
     }
     printf("File created successfully\n");
-    send(nm_socket, "File created successfully\n", 26, 0);
+    // send(nm_socket, "File created successfully\n", 26, 0);
+    send(final_nm_socket, "ERROR 0", 10, 0);
     fclose(file);
 }
 
-void delete_file(int nm_socket, const char *filename) {
+void delete_file( const char *filename) {
     printf("Deleting file: %s\n", filename);
     char buffer[BUFFER_SIZE];
     if (remove(filename) == 0) {
         snprintf(buffer, BUFFER_SIZE, "Deleted file: %s successfully\n", filename);
-        send(nm_socket, buffer, BUFFER_SIZE, 0);
+        printf("Deleted file: %s successfully\n", filename);
+        // send(nm_socket, buffer, BUFFER_SIZE, 0);
+        send(final_nm_socket, "ERROR 0", 10, 0);
     } else {
-        send(nm_socket, "Failed to delete file\n", 22, 0);
+        // send(nm_socket, "Failed to delete file\n", 22, 0);
+        printf("Failed to delete file\n");
+        send(final_nm_socket, "ERROR 6", 10, 0);
     }
 }
 
 
-void create_folder(int nm_socket, const char* filepath){
+void create_folder(const char* filepath){
     char buffer[BUFFER_SIZE];
     memset(buffer, 0, BUFFER_SIZE);
     int status = mkdir(filepath, 0777);
     if (status == -1) {
         snprintf(buffer, sizeof(buffer), "Failed to create folder %s\n", filepath);
-        send(nm_socket, buffer, strlen(buffer), 0);
+        // send(nm_socket, buffer, strlen(buffer), 0);
+        send(final_nm_socket, "ERROR 7", 10, 0);
         printf("Error creating folder %s\n", filepath);
     } else {
         snprintf(buffer, sizeof(buffer), "Folder %s created successfully\n", filepath);
-        send(nm_socket, buffer, strlen(buffer), 0);
+        // send(nm_socket, buffer, strlen(buffer), 0);
+        send(final_nm_socket, "ERROR 0", 10, 0);
         printf("Folder %s created successfully\n", filepath);
     }
 }
 
-void delete_folder(int nm_socket, const char* filepath){
+void delete_folder( const char* filepath){
     printf("Deleting folder: %s\n", filepath);
     DIR* dir = opendir(filepath);
     if (dir == NULL) {
@@ -270,9 +291,9 @@ void delete_folder(int nm_socket, const char* filepath){
         char full_path[MAX_PATH_LEN];
         sprintf(full_path, "%s/%s", filepath, ent->d_name);
         if (ent->d_type == DT_DIR) {
-            delete_folder(nm_socket,full_path);
+            delete_folder(full_path);
         } else {
-            delete_file(nm_socket,full_path);
+            delete_file(full_path);
         }
     }
 
@@ -281,11 +302,14 @@ void delete_folder(int nm_socket, const char* filepath){
     char buffer[BUFFER_SIZE];
     if (status == -1) {
         snprintf(buffer, BUFFER_SIZE, "Failed to delete folder %s\n", filepath);
-        send(nm_socket, buffer, strlen(buffer), 0);
+        // send(nm_socket, buffer, strlen(buffer), 0);
+        send(final_nm_socket, "ERROR 6", 10, 0);
         printf("Error deleting folder %s\n", filepath);
     } else {
-        snprintf(buffer, BUFFER_SIZE, "Folder %s deleted successfully\n", filepath);
-        send(nm_socket, buffer, strlen(buffer), 0);
-        printf("%s",buffer);
+        // snprintf(buffer, BUFFER_SIZE, "Folder %s deleted successfully\n", filepath);
+        // send(nm_socket, buffer, strlen(buffer), 0);
+        send(final_nm_socket, "ERROR 0", strlen("ERROR 0"), 0);
+        // printf("%s",buffer);
+        printf("Folder %s deleted successfully\n", filepath);
     }
 }
