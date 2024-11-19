706e53a8d6019701c69b5ee3bb3a1e7b479f74c8
Viswas-Reddy-PallamReddy <155218470+Viswas-Reddy-PallamReddy@users.noreply.github.com>
2024-11-19 20:08:40 +0530

    Add files via upload

--- StorageServer/headers.h
+++ StorageServer/headers.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ifaddrs.h>
 #include <arpa/inet.h>
 #include <sys/stat.h>
 #include <time.h>
@@ -17,6 +18,7 @@
 #include <errno.h>
 #include <netinet/tcp.h>
 
+extern int final_nm_socket;
 
 #define CHUNK_SIZE 256
 #define BUFFER_SIZE 4096
